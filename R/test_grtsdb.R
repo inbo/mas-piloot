229cf8ee54495c1792ab51ae9ba889e168ac5f80
hansvancalster <hans.vancalster@inbo.be>
2022-02-07 10:55:53 +0100

    test c-mon

--- R/test_grtsdb.R
+++ R/test_grtsdb.R
@@ -5,9 +5,10 @@ library(sf)
 
 plot_sample <- function(sample) {
   sample %>%
+    count(x1c, x2c) %>%
     st_as_sf(coords = c('x1c', 'x2c'), crs = 31370) %>%
     ggplot() +
-    geom_sf()}
+    geom_sf(aes(size = n))}
 
 samplesize  <- 100
 bbox <- rbind(x = c(22000, 258880), y = c(153050, 244030))
@@ -22,7 +23,8 @@ plot_sample(test_10m) # looks OK
 
 add_level(bbox = bbox,
           cellsize = 640,
-          grtsdb = connect_db(here::here("data/c-mon/grts.sqlite"))
+          grtsdb = connect_db(here::here("data/c-mon/grts.sqlite")),
+          level = 9
           )
 # adds levels 14 (20 m), 13, ..., 9 (640 m) to the database
 
@@ -43,4 +45,33 @@ test_40m <- extract_sample(
   cellsize = 40
 )
 
-head(test_40m) # empty, also for all coarser resolutions
+plot_sample(test_40m)
+head(test_40m)
+
+
+library(grtsdb)
+samplesize  <- 100
+bbox <- rbind(x = c(22000, 258880), y = c(153050, 244030))
+db2 <- connect_db(file.path("~", "Downloads", "test.sqlite"))
+test <- extract_sample(
+  grtsdb = db2, samplesize = samplesize, bbox = bbox, cellsize = 500
+)
+plot(test[, 1:2], asp = 1)
+test <- extract_sample(
+  grtsdb = db2, samplesize = samplesize, bbox = bbox, cellsize = 2000
+)
+plot(test[, 1:2], asp = 1)
+compact_db(db2)
+test <- extract_sample(
+  grtsdb = db2, samplesize = samplesize, bbox = bbox, cellsize = 500
+)
+plot(test[, 1:2], asp = 1)
+test <- extract_sample(
+  grtsdb = db2, samplesize = samplesize, bbox = bbox, cellsize = 2000
+)
+plot(test[, 1:2], asp = 1)
+test <- extract_sample(
+  grtsdb = db2, samplesize = samplesize, bbox = bbox, cellsize = 200
+)
+plot(test[, 1:2], asp = 1)
+
