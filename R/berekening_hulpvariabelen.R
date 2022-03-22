a8dc46d11cd2ad5f93418e04c714b3047ad443bf
hansvancalster <hans.vancalster@inbo.be>
2022-03-22 15:08:54 +0100

    beschouw zoekzone in Moeren als prioritair

--- R/berekening_hulpvariabelen.R
+++ R/berekening_hulpvariabelen.R
@@ -128,10 +128,17 @@ path_to_sbp_akkervogels <- function() {
 read_sbp_akkervogels <- function(
   path,
   gebied) {
-  out <- st_read(path) %>%
-    st_transform(crs = 31370) %>%
-    filter(Prioriteit != "Zoekzone") %>%
-    st_intersection(gebied)
+  if (gebied$Naam == "Oostelijke leemstreek") {
+    out <- st_read(path) %>%
+      st_transform(crs = 31370) %>%
+      filter(Prioriteit != "Zoekzone") %>%
+      st_intersection(gebied)
+  }
+  if (gebied$Naam == "De Moeren") {
+    out <- st_read(path) %>%
+      st_transform(crs = 31370) %>%
+      st_intersection(gebied)
+  }
 
   if (nrow(out) >= 1) {
     out <- out %>%
