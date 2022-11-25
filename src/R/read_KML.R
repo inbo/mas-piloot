e6f89ce582e8bfd98a584297f024bce3825ad8a6
wlangera <ward.langeraert@inbo.be>
2022-11-25 10:56:49 +0100

    add documentation function read_KML

--- src/R/read_KML.R
+++ src/R/read_KML.R
@@ -1,16 +1,17 @@
-# dsn = data source name
-# layer = layer name as character string or layer number as number;
-#         if no layer is given, the first layer is loaded
-
+#' Function to properly read in KML file
+#'
+#' @param dsn A data source name: the character string holding the data.
+#' @param layer A layer name as a character string or layer number; in case
+#' layer is missing, read_KML will read the first layer of dsn
 read_KML <- function(dsn, layer = NULL) {
   require(sf)
   require(tidyverse)
-  
+
   # Error handling
   ## File needs to exist
   if (!file.exists(dsn)) {
     stop(paste0("Cannot open ", '"', as.character(dsn), '"',
-                "; The file doesn't seem to exist."), 
+                "; The file doesn't seem to exist."),
          call. = FALSE)
   }
   ## File needs to be a KML file
@@ -21,7 +22,7 @@ read_KML <- function(dsn, layer = NULL) {
   ## If layer name is given, it needs to be to be present in the file
   if (is.character(layer)) {
     if (!(layer %in% st_layers(dsn)$name)) {
-      stop(paste0("Layer ", '"', layer, '"', " not available in file."), 
+      stop(paste0("Layer ", '"', layer, '"', " not available in file."),
            call. = FALSE)
     }
   }
@@ -31,7 +32,7 @@ read_KML <- function(dsn, layer = NULL) {
       stop("Not that many layers in file", call. = FALSE)
     }
   }
-  
+
   # Read in file
   ## First layer if layer == NULL
   if (is.null(layer)) {
@@ -44,10 +45,10 @@ read_KML <- function(dsn, layer = NULL) {
     layer_name <- layer
   }
   kml_file_raw <- st_read(dsn, layer_name)
-  
+
   # Extract variable names in 'Description'
   col_names <- str_match_all(kml_file_raw$Description, "<br>(.*?):")[[1]][, 2]
-  
+
   # Parse to table
   kml_file <- kml_file_raw %>%
     mutate(
@@ -57,7 +58,7 @@ read_KML <- function(dsn, layer = NULL) {
       Description = str_remove_all(Description, "^description:\\s.*?<br>")) %>%
     separate(col = Description, into = col_names, sep = "(<br>)+") %>%
     mutate(across(beschrijving:wie, ~str_extract(., "(?<=:\\s).+")))
-  
+
   # Return
   return(kml_file)
 }
