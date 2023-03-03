2d550f101c4009af9aa41123ea0985a6c42a74cd
wlangera <ward.langeraert@inbo.be>
2023-03-03 13:36:39 +0100

    update meta

--- src/targets/mas_steekproef_zavelberg/_targets_r/targets/hulpvariabelen.R
+++ src/targets/mas_steekproef_zavelberg/_targets_r/targets/hulpvariabelen.R
@@ -3,12 +3,17 @@ list(
     name = plus_sb,
     command = add_bo_to_frame(
       punten_df = punten_selectie_zichtbaarheid,
-      path_bo = bo_file)
-    ),
+      path_bo = bo_file
+      ),
+    pattern = map(punten_selectie_zichtbaarheid)
+  ),
   tar_target(
     name = lbg_statistics,
-    command = calc_lbg(path = lbg_file,
-                       punten_sf = punten_selectie_zichtbaarheid)
+    command = calc_lbg(
+      path = lbg_file,
+      punten_sf = punten_selectie_zichtbaarheid
+      ),
+    pattern = map(punten_selectie_zichtbaarheid)
   ),
   tar_target(
     name = plus_openheid_landschap,
@@ -17,20 +22,27 @@ list(
       punten_sf = plus_sb,
       gebied = perimeters_data,
       cutlevels = c(1.25, 1.35, 1.51),
-      class_labels = c("GL", "HGL", "HOL", "OL"))
+      class_labels = c("GL", "HGL", "HOL", "OL")
+      ),
+    pattern = map(perimeters_data, plus_sb)
   ),
-  tar_target(name = sbp_akkervogels,
-             command = read_sbp_akkervogels(
-               path = sbp_akkervogels_file,
-               gebied = perimeters_data,
-               path_extra_soorten = sbp_overige_file,
-               extra_soorten = c("hamster", "bruine kiekendief", "zomertortel", 
-                                 "grauwe kiekendief")
-             )
+  tar_target(
+    name = sbp_akkervogels,
+    command = read_sbp_akkervogels(
+      path = sbp_akkervogels_file,
+      gebied = perimeters_data,
+      path_extra_soorten = sbp_overige_file,
+      extra_soorten = c("hamster", "bruine kiekendief", "zomertortel", 
+                        "grauwe kiekendief")
+      ),
+    pattern = map(perimeters_data)
   ),
   tar_target(
     name = steekproefkader_finaal,
-    command = add_stratum_sbp(punten_sf = plus_openheid_landschap,
-                              sbp = sbp_akkervogels)
+    command = add_stratum_sbp(
+      punten_sf = plus_openheid_landschap,
+      sbp = sbp_akkervogels
+      ),
+    pattern = map(sbp_akkervogels, plus_openheid_landschap)
   )
 )
--- src/targets/mas_steekproef_zavelberg/_targets_r/targets/steekproefkader.R
+++ src/targets/mas_steekproef_zavelberg/_targets_r/targets/steekproefkader.R
@@ -1,73 +1,81 @@
 list(
-  tar_target(name = selectie_openheid_klasses,
-             command = selectie_openheid(
-               gebied = perimeters_data,
-               ol_strata = c("OL", "HOL")
-             ),
-             pattern = map(perimeters_data)
+  tar_target(
+    name = selectie_openheid_klasses,
+    command = selectie_openheid(
+      gebied = perimeters_data,
+      ol_strata = c("OL", "HOL")
+      ),
+    pattern = map(perimeters_data)
   ),
-  tar_target(name = exclusie_osm_landgebruiken,
-             command = exclusie_landgebruik_osm(
-               gebied = selectie_openheid_klasses,
-          	   osmdata = osm_belgium,
-          	   landuse = c("residential", "military", "industrial", "cemetery", 
-          	               "railway", "commercial", "farmyard"),
-          	   leisure = c("park"),
-          	   buffer_poly = 0, 
-          	   layer_poly = list(aeroway = c("aerodrome")),
-          	   buffer_line = 50, 
-          	   layer_line = list(highway = c("motorway", "motorway_link")),
-               update_osm_layer = FALSE
-               ),
-             pattern = map(selectie_openheid_klasses)
+  tar_target(
+    name = exclusie_osm_landgebruiken,
+    command = exclusie_landgebruik_osm(
+      gebied = selectie_openheid_klasses,
+      osmdata = osm_belgium,
+      landuse = c("residential", "military", "industrial", "cemetery", 
+                  "railway", "commercial", "farmyard"),
+      leisure = c("park"),
+      buffer_poly = 0, 
+      layer_poly = list(aeroway = c("aerodrome")),
+      buffer_line = 50, 
+      layer_line = list(highway = c("motorway", "motorway_link")),
+      update_osm_layer = FALSE
+      ),
+    pattern = map(selectie_openheid_klasses)
   ),
-  tar_target(name = paden,
-             command = extract_osm_paden(
-               gebied = selectie_openheid_klasses,
-               exclusie = exclusie_osm_landgebruiken,
-               osmdata = osm_belgium,
-               paths_include = c('track', 'footway', 'path', 'cycleway', 
-                'bridleway', 'tertiary', 'tertiary_link', 'unclassified'),
-               cutting_exclude = NULL,
-               historic_exclude = NULL,
-               waterway = NULL,
-               update_osm_layer = FALSE
-               ),
-             pattern = map(selectie_openheid_klasses, exclusie_osm_landgebruiken)
-             ),
-  tar_target(name = punten,
-             command = paden_naar_punten(
-               data_paden = paden,
-               gebieden = perimeters_data,
-               interpoint_distance = 50,
-               border_distance = 300
-               ),
-             pattern = map(perimeters_data, paden)
-             ) ,
-  tar_target(name = telcirkels_landgebruik,
-             command = punten_lum_buffer(
-               punten_sf = punten,
-               radius = 300,
-               file = vito_lum_2019_file,
-               legend = legend_lum
-              ),
-             pattern = map(punten)),
-  tar_target(name = telcirkels_selectie_landgebruik,
-             command = punten_selectie_landgebruik(
-               lum_extract_result = telcirkels_landgebruik,
-               legend_rast = legend_lum,
-               max_prop_overige = 0.5,
-               min_prop_akker = 0.3,
-               min_prop_akker_grasland = 0.4
-               ),
-             pattern = map(telcirkels_landgebruik)
+  tar_target(
+    name = paden,
+    command = extract_osm_paden(
+      gebied = selectie_openheid_klasses,
+      exclusie = exclusie_osm_landgebruiken,
+      osmdata = osm_belgium,
+      paths_include = c('track', 'footway', 'path', 'cycleway', 
+      'bridleway', 'tertiary', 'tertiary_link', 'unclassified'),
+      cutting_exclude = NULL,
+      historic_exclude = NULL,
+      waterway = NULL,
+      update_osm_layer = FALSE
+      ),
+    pattern = map(selectie_openheid_klasses, exclusie_osm_landgebruiken)
   ),
-  tar_target(name = selectie_landgebruik,
-             command = selectie_landgebruik_vito(
-               punten_sf = punten,
-               selectie_df = telcirkels_selectie_landgebruik
-             ),
-             pattern = map(punten, telcirkels_selectie_landgebruik)
+  tar_target(
+    name = punten,
+    command = paden_naar_punten(
+      data_paden = paden,
+      gebieden = perimeters_data,
+      interpoint_distance = 50,
+      border_distance = 300
+      ),
+    pattern = map(perimeters_data, paden)
+  ) ,
+  tar_target(
+    name = telcirkels_landgebruik,
+    command = punten_lum_buffer(
+      punten_sf = punten,
+      radius = 300,
+      file = vito_lum_2019_file,
+      legend = legend_lum
+      ),
+    pattern = map(punten)
+  ),
+  tar_target(
+    name = telcirkels_selectie_landgebruik,
+      command = punten_selectie_landgebruik(
+        lum_extract_result = telcirkels_landgebruik,
+        legend_rast = legend_lum,
+        max_prop_overige = 0.5,
+        min_prop_akker = 0.3,
+        min_prop_akker_grasland = 0.4
+        ),
+    pattern = map(telcirkels_landgebruik)
+  ),
+  tar_target(
+    name = selectie_landgebruik,
+    command = selectie_landgebruik_vito(
+      punten_sf = punten,
+      selectie_df = telcirkels_selectie_landgebruik
+      ),
+    pattern = map(punten, telcirkels_selectie_landgebruik)
   ),
   tarchetypes::tar_group_size(
     name = selectie_landgebruik_per_size,
@@ -83,11 +91,17 @@ list(
     ),
     pattern = map(selectie_landgebruik_per_size)
   ),
+  tarchetypes::tar_group_by(
+    name = punten_zichtbaarheid_per_regio,
+    command = punten_zichtbaarheid,
+    Naam
+  ),
   tar_target(
     name = punten_selectie_zichtbaarheid,
     command = filter_zichtbaarheid(
-      punten_sf = punten_zichtbaarheid,
+      punten_sf = punten_zichtbaarheid_per_regio,
       min_cvvi = 0.1
-    )
+    ),
+    pattern = map(punten_zichtbaarheid_per_regio)
   )
 )
