389123ae98a840e4df0732de88d78869adee1b4d
wlangera <ward.langeraert@inbo.be>
2023-03-17 16:13:43 +0100

    update meta

--- src/targets/mas_steekproef_zavelberg/_targets_r/targets/version-control.R
+++ src/targets/mas_steekproef_zavelberg/_targets_r/targets/version-control.R
@@ -3,7 +3,7 @@ list(
     output_objecten,
     output_finaal(
       files = list(steekproefkader = steekproefkader_finaal, 
-                   steekproef = steekproef_final_300),
+                   steekproef = steekproef_final_600),
       write_out = FALSE
     )
   )
