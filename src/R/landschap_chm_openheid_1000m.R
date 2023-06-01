6ed6f3eca7fcabb9b96b92611d4d3f723b12d206
wlangera <ward.langeraert@inbo.be>
2023-06-01 17:26:45 +0200

    adjust to 1000 m

--- src/R/landschap_chm_openheid_1000m.R
+++ src/R/landschap_chm_openheid_1000m.R
@@ -12,8 +12,8 @@ vlaanderen_chm5[vlaanderen_chm5 < 0] <- 0
 result <- qgis_run_algorithm(
   "saga:topographicopenness",
   DEM = vlaanderen_chm5,
-  POS = here::here("data", "dem", "openness300m_chm_res5_vlaanderen.tif"),
-  RADIUS = 300,
+  POS = here::here("data", "dem", "openness1000m_chm_res5_vlaanderen.tif"),
+  RADIUS = 1000,
   METHOD = 1,
   DLEVEL = 3,
   NDIRS = 8,
@@ -24,57 +24,39 @@ result <- qgis_run_algorithm(
 # Flat area will return 1.57 (radians) = 90°
 # Smaller angles = less open
 # Larger angles = elevated position in landscape
-top_open_300m <- terra::rast(
-  here::here("data", "dem", "openness300m_chm_res5_vlaanderen.tif"))
+top_open_1000m <- terra::rast(
+  here::here("data", "dem", "openness1000m_chm_res5_vlaanderen.tif"))
 
 # aggregate to 25 m resolution
-top_open_300m_res25 <- aggregate(
-  top_open_300m, fact = 5,
-  filename = here::here("data", "dem", "openness300m_chm_res25_vlaanderen.tif"))
+top_open_1000m_res25 <- aggregate(
+  top_open_1000m, fact = 5,
+  filename = here::here("data", "dem", "openness1000m_chm_res25_vlaanderen.tif"))
 
-top_open_300m_res25 <-  terra::rast(
-  here::here("data", "dem", "openness300m_chm_res25_vlaanderen.tif"))
+top_open_1000m_res25 <-  terra::rast(
+  here::here("data", "dem", "openness1000m_chm_res25_vlaanderen.tif"))
 
-
-# # next calculated average openness in 1000m radius landscape
-# circle1000 <- focalMat(top_open_300m_res25,
-#                        d = 1000,
-#                        type = "circle",
-#                        fillNA = TRUE)
-#
-#
-# openness300m_chm_res25_c1000_mean_vlaanderen <- terra::focal(
-#   top_open_300m_res25,
-#   w = circle1000,
-#   fun = "sum",
-#   na.rm = TRUE,
-#   filename = here("data", "dem",
-#                   "openness300m_chm_res25_c1000_mean_vlaanderen.tif"),
-#   overwrite = FALSE)
-#
-
-# next calculated average openness in 300m radius landscape
-circle300 <- focalMat(top_open_300m_res25,
-                       d = 300,
+# next calculated average openness in 1000m radius landscape
+circle1000 <- focalMat(top_open_1000m_res25,
+                       d = 1000,
                        type = "circle",
                        fillNA = TRUE)
 
 
-openness300m_chm_res25_c300_mean_vlaanderen <- terra::focal(
-  top_open_300m_res25,
-  w = circle300,
+openness1000m_chm_res25_c1000_mean_vlaanderen <- terra::focal(
+  top_open_1000m_res25,
+  w = circle1000,
   fun = "sum",
   na.rm = TRUE,
   filename = here("data", "dem",
-                  "openness300m_chm_res25_c300_mean_vlaanderen.tif"),
+                  "openness1000m_chm_res25_c1000_mean_vlaanderen.tif"),
   overwrite = FALSE)
 
 
 
-openness300m_chm_res25_c300_mean_vlaanderen <-
+openness1000m_chm_res25_c1000_mean_vlaanderen <-
   rast(
     here("data", "dem",
-         "openness300m_chm_res25_c300_mean_vlaanderen.tif")
+         "openness1000m_chm_res25_c1000_mean_vlaanderen.tif")
   )
 
 rclasmat <- c(0, 1.25, 1,
@@ -83,20 +65,12 @@ rclasmat <- c(0, 1.25, 1,
               1.51, +Inf, 4) %>%
   matrix(ncol = 3, byrow = TRUE)
 
-# vlaanderen_openheid_landschap <- terra::classify(
-#   openness300m_chm_res25_c1000_mean_vlaanderen,
-#   rcl = rclasmat,
-#   include.lowest = TRUE,
-#   filename = here("data", "dem",
-#                   "openness300m_chm_res25_c1000_mean_vlaanderen_classified.tif"),
-#   overwrite = TRUE)
-
 vlaanderen_openheid_landschap <- terra::classify(
-  openness300m_chm_res25_c300_mean_vlaanderen,
+  openness1000m_chm_res25_c1000_mean_vlaanderen,
   rcl = rclasmat,
   include.lowest = TRUE,
   filename = here("data", "dem",
-                  "openness300m_chm_res25_c300_mean_vlaanderen_classified.tif"),
+                  "openness1000m_chm_res25_c1000_mean_vlaanderen_classified.tif"),
   overwrite = TRUE)
 
 
